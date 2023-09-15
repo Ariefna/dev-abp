b34a71e1f2e4c562a8054f7e80ca5003e91a2ced
Reza Prayogo <reza.prasetya.prayogo@gmail.com>
2023-09-16 00:23:03 +0700

    tabel detail dooring

--- app/Models/DetailTracking.php
+++ app/Models/DetailTracking.php
@@ -10,4 +10,9 @@ class DetailTracking extends Model
     use HasFactory;
     protected $fillable = ['id_track','id_gudang','id_kapal', 'qty_tonase', 'qty_timbang','jml_sak','nopol','no_container','voyage', 'no_segel','tgl_muat','td','td_jkt','ta','no_sj','sj_file_name','sj_file_path','st_file_name','st_file_path','status'];
     protected $table = 'detail_tracking';    
+
+    public function docTracking()
+    {
+        return $this->belongsTo(DocTracking::class,'id_track','id_track');
+    }
 }
