7279dce10f6bd836b374d14f54d2043476f6e695
Ariefna <79347655+Ariefna@users.noreply.github.com>
2023-09-25 00:49:14 +0700

    hak akses

--- resources/views/pages/abp-page/mgudangmuat.blade.php
+++ resources/views/pages/abp-page/mgudangmuat.blade.php
@@ -122,9 +122,18 @@
                                             <td>{{ $gd->kota }}</td>
                                             <td class="text-center">
                                                 <ul class="table-controls">
-                                                    <li><a href="#exampleModal-{{ $gd->id_gudang }}" class="bs-tooltip" data-bs-toggle="modal" data-bs-placement="top" title="Edit" data-original-title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 p-1 br-8 mb-1"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a></li>
-                                                    <li><a href="#exampleModalHps-{{ $gd->id_gudang }}" class="bs-tooltip" data-bs-toggle="modal" data-bs-placement="top" title="Delete" data-original-title="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash p-1 br-8 mb-1"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a></li>
-                                                </ul>
+                                                @if(in_array('master-gudang-muat-UPDATE', Session::get('nama_action')) || Session::get('role') == 'superadmin')
+                                                                <li><a href="#exampleModal-{{ $gd->id_gudang }}" class="bs-tooltip" data-bs-toggle="modal" data-bs-placement="top" title="Edit" data-original-title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 p-1 br-8 mb-1">
+                                                                            <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+                                                                        </svg></a></li>
+                                                                @endif
+                                                                @if(in_array('master-gudang-muat-DELETE', Session::get('nama_action')) || Session::get('role') == 'superadmin')
+                                                                <li><a href="#exampleModalHps-{{ $gd->id_gudang }}" class="bs-tooltip" data-bs-toggle="modal" data-bs-placement="top" title="Delete" data-original-title="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash p-1 br-8 mb-1">
+                                                                            <polyline points="3 6 5 6 21 6"></polyline>
+                                                                            <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                                                                        </svg></a></li>
+                                                                @endif
+                                                 </ul>
                                             </td>
                                         </tr>
                                         @endforeach
