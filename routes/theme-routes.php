9b1f1b7e5928cc5e1a996fe8aba472933666dfa6
pawsley <ilhamirwansyah12@gmail.com>
2023-09-25 17:10:24 +0700

    routes

--- routes/theme-routes.php
+++ routes/theme-routes.php
@@ -68,30 +68,6 @@ foreach ($prefixRouters as $prefixRouter) {
                 })->name('customer.index');
                 Route::resource('/customer', \App\Http\Controllers\CustomerController::class);
                 // Route::put('/customer/{customer}', [CustomerController::class, 'update'])->name('customer.update');
-
-<<<<<<< Updated upstream
-        /**
-         * ==============================
-         *       @Router -  Monitoring
-         * ==============================
-         */
-        
-        Route::prefix('monitoring')->group(function () {
-            Route::get('/mon-tracking', function () {
-                return view('pages.abp-page.montracking', ['title' => 'Adhipramana Bahari Perkasa', 'breadcrumb' => 'This Breadcrumb']);
-            })->name('mon-tracking.index');
-            Route::get('/mon-tracking/print/{id_detail_track}', [\App\Http\Controllers\MTrackingController::class, 'print'])->name('monitoring.tracking.print');
-            Route::get('/mon-tracking/print/spk/{id_detail_track}', [\App\Http\Controllers\MTrackingController::class, 'printSPK'])->name('monitoring.tracking.spk');
-            Route::resource('/mon-tracking', \App\Http\Controllers\MTrackingController::class);
-            Route::put('/mon-tracking', [\App\Http\Controllers\MTrackingController::class, 'update'])->name('mon-tracking.update');
-            // Route::put('edit/{id}','ProductController@update')->name('product.update');
-            // Route::match(['post'], '/mon-tracking/mt/tambahdt', [\App\Http\Controllers\MTrackingController::class, 'tambahdt'])->name('mon-tracking.tambahdt');
-            Route::get('/mon-dooring', function () {
-                return view('pages.abp-page.mondooring', ['title' => 'Adhipramana Bahari Perkasa', 'breadcrumb' => 'This Breadcrumb']);
-            })->name('mon-dooring.index');
-            Route::resource('/mon-dooring', \App\Http\Controllers\MDooringController::class);
-        });
-=======
                 // Route::post('/data-insert', [CustomerController::class, 'store'])->name('data.insert');
                 // Route::resource('customer', ); 
                 Route::get('/barang', function () {
