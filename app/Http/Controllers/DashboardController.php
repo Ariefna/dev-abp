124b2979a1bd65cc0782ff4665724487cff75bc4
Ariefna <79347655+Ariefna@users.noreply.github.com>
2023-11-13 23:23:13 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,13 +7,14 @@ use App\Models\DocTracking;
 use App\Models\DocDooring;
 use App\Models\InvoiceDP;
 use App\Models\InvoicePelunasan;
+use App\Models\PurchaseOrder;
 use App\Http\Controllers\DB;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         $tbl_po = DocTracking::select('doc_tracking.no_po', 'purchase_orders.po_kebun', 
                 'purchase_orders.total_qty', 'port_of_loading.nama_pol', 'port_of_destination.nama_pod',
                 'detail_tracking.status', 'kapals.kode_kapal','kapals.nama_kapal','pt_penerima.nama_penerima',
@@ -76,27 +77,37 @@ class DashboardController extends Controller
                 ->groupBy('detail_dooring_sisa.id_dooring')
                 ->get();                
         $currentMonth = Carbon::now()->month;
-        $datagrafik = [];
+        $datagrafikPendapatan = [];
+        $datagrafikBiaya = [];
+        $datagrafikProfit = [];
         // $totaldp = InvoiceDp::where('status', 2)->whereYear('invoice_date', $currentYear)->sum('total_invoice');
         // $totalpel = InvoicePelunasan::whereIn('status', [2,3])->whereYear('invoice_date', $currentYear)->sum('total_invoice');
         // $totalpo = PurchaseOrder::whereIn('status',[2])->sum('total_all');
         $totaldp = InvoiceDp::where('status', 2)->whereMonth('invoice_date', $currentMonth)->sum('total_invoice');
         $totalpel = InvoicePelunasan::whereIn('status', [2,3])->whereMonth('invoice_date', $currentMonth)->sum('total_invoice');
-        $totalpo = PurchaseOrder::whereIn('status',[2])->whereMonth('tgl', $currentMonth)->sum('total_all');        
+        $totalpo = PurchaseOrder::whereIn('status',[2])->whereMonth('tgl', $currentMonth)->sum('total_all');   
+        // Get the value of the 'yearFilter' parameter from the request
+        $selectedYear = $request->input('yearFilter');
 
-        for ($i = 0; $i < 10; $i++) {
-            $year = $currentYear + $i;
-            
-            $dp = InvoiceDp::where('status', 2)->whereYear('invoice_date', $year)->sum('total_invoice');
-            $pel = InvoicePelunasan::whereIn('status', [2,3])->whereYear('invoice_date', $year)->sum('total_invoice');
-            
-            $datagrafik[$year] = $dp + $pel;
+        // If the parameter is not set, use the current year as a default
+        $currentYear = $selectedYear ?: date('Y');     
+        for ($bulan = 1; $bulan <= 12; $bulan++) {
+            $dp = InvoiceDp::where('status', 2)
+                            ->whereYear('invoice_date', $currentYear)
+                            ->whereMonth('invoice_date', $bulan)
+                            ->sum('total_invoice');
+        
+            $pel = InvoicePelunasan::whereIn('status', [2, 3])
+                                    ->whereYear('invoice_date', $currentYear)
+                                    ->whereMonth('invoice_date', $bulan)
+                                    ->sum('total_invoice');
+        
+            $datagrafikPendapatan["$currentYear-$bulan"] = $dp + $pel;
         }
-        // dd($datagrafik);
         $title = 'Adhipramana Bahari Perkasa';
         $breadcrumb = 'This Breadcrumb';
         return view('pages.dashboard.analytics', compact('title', 'breadcrumb',
-        'tbl_po','tbl_dor','totaldp','totalpel','datagrafik','totalpo'));        
+        'tbl_po','tbl_dor','totaldp','totalpel','datagrafikPendapatan','totalpo'));        
     }
 
     public function addsisatrack(Request $request, $no_po) {
