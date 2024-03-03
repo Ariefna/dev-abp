9e6911d815834a1ea9b603ab389f7dea9f14e990
Ariefna <79347655+Ariefna@users.noreply.github.com>
2024-03-03 21:37:36 +0700

    distict

--- app/Http/Controllers/MTrackingController.php
+++ app/Http/Controllers/MTrackingController.php
@@ -50,31 +50,32 @@ class MTrackingController extends Controller
                 ->where('purchase_orders.status', '=', 2)
                 ->get();
         $tbl_po = DocTracking::select('doc_tracking.no_po', 'purchase_orders.po_kebun', 'penerimas.estate',
-                'purchase_orders.total_qty', 'port_of_loading.id as id_pol','port_of_loading.nama_pol','port_of_destination.id as id_pod', 'port_of_destination.nama_pod',
-                'detail_tracking.status', 'kapals.id as id_kapal','kapals.kode_kapal','kapals.nama_kapal','pt_penerima.nama_penerima',
-                'gudang_muats.nama_gudang','gudang_muats.id_gudang', 'barangs.nama_barang','purchase_orders.no_pl', 'detail_tracking.tgl_muat',
-                'purchase_orders.po_kebun','detail_tracking.qty_tonase', 'detail_tracking.qty_timbang','detail_tracking.jml_sak',
-                'detail_tracking.nopol','detail_tracking.no_container','detail_tracking.voyage','detail_tracking.td','detail_tracking.td_jkt',
-                'detail_tracking.ta','customers.nama_customer','doc_tracking.status_kapal','detail_tracking_sisa.qty_tonase_sisa','detail_tracking_sisa.tipe',
-                'doc_tracking.id_track', 'detail_tracking.id_detail_track','detail_tracking.track_file','detail_tracking.door_file'
-                ,'detail_tracking.sj_file_name','detail_tracking.st_file_name', 'detail_tracking.harga_hpp', 'detail_tracking.no_sj', 'detail_tracking.no_segel')
-                ->join('detail_tracking','detail_tracking.id_track','=','doc_tracking.id_track')
-                ->join('detail_tracking_sisa','detail_tracking_sisa.id_track','=','doc_tracking.id_track')
-                ->join('gudang_muats', 'gudang_muats.id_gudang', '=', 'detail_tracking.id_gudang')
-                ->join('purchase_orders', 'purchase_orders.po_muat', '=', 'doc_tracking.no_po')
-                ->join('port_of_loading', 'port_of_loading.id', '=', 'detail_tracking.id_pol')
-                ->join('port_of_destination', 'port_of_destination.id', '=', 'detail_tracking.id_pod')
-                ->join('kapals', 'kapals.id', '=', 'detail_tracking.id_kapal')
-                ->join('detail_p_h_s', 'purchase_orders.id_detail_ph', '=', 'detail_p_h_s.id_detail_ph')
-                ->join('penawaran_hargas', 'penawaran_hargas.id_penawaran', '=', 'detail_p_h_s.id_penawaran')
-                ->join('customers', 'customers.id', '=', 'penawaran_hargas.id_customer')
-                ->join('penerimas', 'detail_p_h_s.id_penerima', '=', 'penerimas.id_penerima')
-                ->join('pt_penerima', 'pt_penerima.id_pt_penerima', '=', 'penerimas.id_pt_penerima')
-                ->join('barangs', 'purchase_orders.id', '=', 'barangs.id')
-                ->whereIn('doc_tracking.status', [2,3,4])
-                ->whereIn('detail_tracking.status', [2,3,4])
-                ->whereNotIn('purchase_orders.status',[0])
-                ->get();
+'purchase_orders.total_qty', 'port_of_loading.id as id_pol','port_of_loading.nama_pol','port_of_destination.id as id_pod', 'port_of_destination.nama_pod',
+'detail_tracking.status', 'kapals.id as id_kapal','kapals.kode_kapal','kapals.nama_kapal','pt_penerima.nama_penerima',
+'gudang_muats.nama_gudang','gudang_muats.id_gudang', 'barangs.nama_barang','purchase_orders.no_pl', 'detail_tracking.tgl_muat',
+'purchase_orders.po_kebun','detail_tracking.qty_tonase', 'detail_tracking.qty_timbang','detail_tracking.jml_sak',
+'detail_tracking.nopol','detail_tracking.no_container','detail_tracking.voyage','detail_tracking.td','detail_tracking.td_jkt',
+'detail_tracking.ta','customers.nama_customer','doc_tracking.status_kapal','detail_tracking_sisa.tipe',
+'doc_tracking.id_track', 'detail_tracking.id_detail_track','detail_tracking.track_file','detail_tracking.door_file'
+,'detail_tracking.sj_file_name','detail_tracking.st_file_name', 'detail_tracking.harga_hpp', 'detail_tracking.no_sj', 'detail_tracking.no_segel')
+->join('detail_tracking','detail_tracking.id_track','=','doc_tracking.id_track')
+->join('detail_tracking_sisa','detail_tracking_sisa.id_track','=','doc_tracking.id_track')
+->join('gudang_muats', 'gudang_muats.id_gudang', '=', 'detail_tracking.id_gudang')
+->join('purchase_orders', 'purchase_orders.po_muat', '=', 'doc_tracking.no_po')
+->join('port_of_loading', 'port_of_loading.id', '=', 'detail_tracking.id_pol')
+->join('port_of_destination', 'port_of_destination.id', '=', 'detail_tracking.id_pod')
+->join('kapals', 'kapals.id', '=', 'detail_tracking.id_kapal')
+->join('detail_p_h_s', 'purchase_orders.id_detail_ph', '=', 'detail_p_h_s.id_detail_ph')
+->join('penawaran_hargas', 'penawaran_hargas.id_penawaran', '=', 'detail_p_h_s.id_penawaran')
+->join('customers', 'customers.id', '=', 'penawaran_hargas.id_customer')
+->join('penerimas', 'detail_p_h_s.id_penerima', '=', 'penerimas.id_penerima')
+->join('pt_penerima', 'pt_penerima.id_pt_penerima', '=', 'penerimas.id_pt_penerima')
+->join('barangs', 'purchase_orders.id', '=', 'barangs.id')
+->whereIn('doc_tracking.status', [2,3,4])
+->whereIn('detail_tracking.status', [2,3,4])
+->whereNotIn('purchase_orders.status',[0])
+->distinct()
+->get();
         $trackzero = DocTracking::select('*', 'doc_tracking.status', 'doc_tracking.id_track')
                 ->selectSub(function ($query) {
                     $query->selectRaw('SUM(qty_tonase)')
